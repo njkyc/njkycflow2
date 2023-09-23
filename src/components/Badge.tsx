d784329296e60ee45ef040ebbd10cd757e34a158
njkyc <nj_k@yahoo.com>
2023-09-23 12:53:49 -0400

    change-01

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,17 +51,17 @@ export const Badge = (props: Props) => {
       "text-align": 'center',
       color: props.poweredByTextColor ?? defaultTextColor,
       "background-color": props.badgeBackgroundColor ?? '#ffffff'
-    }}>Powered by
+    }}>Made by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://google.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Nohjin</span>
       </a>
     </span>
   )
